55c9f92ac4b167b2ac890b2a8b6628cf65c17b50
Evan You <yyx990803@gmail.com>
2021-06-18 14:06:56 -0400

    avoid loading carbon ads on mobile

--- src/vitepress/components/VPCarbonAds.vue
+++ src/vitepress/components/VPCarbonAds.vue
@@ -1,19 +1,38 @@
 <script setup lang="ts">
+import { useMediaQuery } from '@vueuse/core'
 import { useData } from 'vitepress'
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, watch } from 'vue'
 
 const { theme } = useData()
 const carbonOptions = theme.value.carbonAds
 const container = ref()
+const isWide = useMediaQuery('(min-width: 1280px)')
 
-// no need to account for option changes during dev, we can just refresh the page
-if (carbonOptions) {
-  onMounted(() => {
+let hasInitalized = false
+
+function init() {
+  if (!hasInitalized) {
+    console.log('init carbon')
+    hasInitalized = true
     const s = document.createElement('script')
     s.id = '_carbonads_js'
     s.src = `//cdn.carbonads.com/carbon.js?serve=${carbonOptions.code}&placement=${carbonOptions.placement}`
     s.async = true
     container.value.appendChild(s)
+  }
+}
+
+// no need to account for option changes during dev, we can just refresh the page
+if (carbonOptions) {
+  onMounted(() => {
+    // if the page is loaded in wide mode, load carbon directly.
+    // otherwise, only load it if the page resizes to wide enough.
+    // this avoids loading carbon at all on mobile where it's never shown.
+    if (isWide.value) {
+      init()
+    } else {
+      watch(isWide, (wide) => wide && init())
+    }
   })
 }
 </script>
