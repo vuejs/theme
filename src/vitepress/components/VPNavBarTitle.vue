296ed339664a8f38d341fe926aa34ff0dbb95da7
Evan You <yyx990803@gmail.com>
2021-08-18 15:38:34 -0400

    simplify logo and remove logoDark for now

--- src/vitepress/components/VPNavBarTitle.vue
+++ src/vitepress/components/VPNavBarTitle.vue
@@ -5,7 +5,6 @@ import { useData } from 'vitepress'
 const { site, localePath } = useData()
 
 const logo = computed(() => site.value.themeConfig.logo)
-const logoDark = computed(() => site.value.themeConfig.logoDark)
 
 const title = computed(() => {
   if (site.value.themeConfig.title === null) {
@@ -22,22 +21,8 @@ const hasTitle = computed(() => {
 
 <template>
   <a v-if="hasTitle" class="VPNavBarTitle" :href="localePath">
-    <div v-if="logo" class="logo">
-      <img
-        class="logo-img"
-        :class="{ 'has-dark': logoDark }"
-        :src="logo"
-        alt="Logo"
-      />
-      <img
-        v-if="logoDark"
-        class="logo-img inverse"
-        :src="logoDark"
-        alt="Logo"
-      />
-    </div>
-
-    <p v-if="title" class="text">{{ title }}</p>
+    <img v-if="logo" class="logo" :src="logo" alt="Logo" />
+    <span v-if="title" class="text">{{ title }}</span>
   </a>
 </template>
 
@@ -56,36 +41,12 @@ const hasTitle = computed(() => {
 
 .logo {
   position: relative;
+  width: 24px;
   height: 24px;
 }
 
 .logo + .text {
   padding-left: 8px;
-}
-
-.logo-img {
-  height: 100%;
-  transition: opacity 0.5s, transform 0.5s ease;
-}
-
-.logo-img.inverse {
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  opacity: 0;
-  transform: translateY(-12px);
-}
-
-.dark .logo-img.has-dark {
-  opacity: 0;
-  transform: translateY(12px);
-}
-
-.dark .logo-img.inverse {
-  opacity: 1;
-  transform: translateY(0);
 }
 
 .text {
--- src/vitepress/config.ts
+++ src/vitepress/config.ts
@@ -20,15 +20,6 @@ export interface Config {
   logo?: string
 
   /**
-   * The path to the dark mode logo file. If it's undefined, still `logo` file
-   * will be used in dark mode. If `logo` undefined, the option will be
-   * ignored as well.
-   *
-   * @example `'/img/logo-dark.svg'`
-   */
-  logoDark?: string
-
-  /**
    * The title text to be displayed on the navbar. If `logo` is set, the title
    * will be displayed right next to the logo. Set `null` to disable.
    *
