4772600b89a1b23b9a607c8d31592bf1056f170a
Evan You <yyx990803@gmail.com>
2021-12-17 11:43:32 +0800

    inline logo to avoid request

--- src/vitepress/components/VPNavBarTitle.vue
+++ src/vitepress/components/VPNavBarTitle.vue
@@ -21,14 +21,16 @@ const hasTitle = computed(() => {
 
 <template>
   <a v-if="hasTitle" class="VPNavBarTitle" :href="localePath">
-    <img
-      v-if="logo"
-      class="logo"
-      width="24"
-      height="24"
-      :src="logo"
-      alt="Logo"
-    />
+    <svg class="logo" viewBox="0 0 128 128" width="24" height="24">
+      <path
+        fill="#42b883"
+        d="M78.8,10L64,35.4L49.2,10H0l64,110l64-110C128,10,78.8,10,78.8,10z"
+      />
+      <path
+        fill="#35495e"
+        d="M78.8,10L64,35.4L49.2,10H25.6L64,76l38.4-66H78.8z"
+      />
+    </svg>
     <span v-if="title" class="text">{{ title }}</span>
   </a>
 </template>
