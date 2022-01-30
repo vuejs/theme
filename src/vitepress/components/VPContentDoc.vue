35d19824583c2fd8425a56e88d83187bfe009e80
Enzo Innocenzi <enzo@innocenzi.dev>
2022-01-30 22:36:00 +0100

    fix: remove hardcoded repository from edit link

--- src/vitepress/components/VPContentDoc.vue
+++ src/vitepress/components/VPContentDoc.vue
@@ -14,7 +14,7 @@ const hashMatch = /#(\w+)$/
 const repoUrl = computed(() => {
   const repo = theme.value.editLink?.repo || 'vuejs/docs'
   const branch = repo.match(hashMatch)?.[1] || 'main'
-  return `https://github.com/vuejs/docs/edit/${branch}/src/${page.value.relativePath}`
+  return `https://github.com/${repo}/edit/${branch}/src/${page.value.relativePath}`
 })
 
 const pageClass = computed(() => {
