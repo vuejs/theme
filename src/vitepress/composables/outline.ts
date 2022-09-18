8eddc8f1f92eab93ae8f5a7ad9e27ab19fc9922b
Jinjiang <zhaojinjiang@me.com>
2022-09-18 13:10:43 +0800

    Update outline.ts

--- src/vitepress/composables/outline.ts
+++ src/vitepress/composables/outline.ts
@@ -53,7 +53,7 @@ export function useActiveAnchor(
     // page bottom - highlight last one
     if (
       anchors.length &&
-      window.scrollY + window.innerHeight >= document.body.offsetHeight
+      window.scrollY + window.innerHeight >= document.body.offsetHeight - 1
     ) {
       activateLink(anchors[anchors.length - 1].hash)
       return
